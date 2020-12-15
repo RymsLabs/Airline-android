472c8aa24859628fc4e7b2125cb3e2407d04ac0f
Sahajpal <sahajpal80@gmail.com>
2020-12-15 13:41:10 +0530

    Completed Login for all users

---
+++ app/src/main/java/com/ryms/airlinemanagement/Config.java
@@ -0,0 +1,9 @@
+package com.ryms.airlinemanagement;
+
+public class Config {
+    public static final String BASE_URL = "https://airline-ryms.herokuapp.com/";
+
+    public static final String USER_LOGIN = BASE_URL+"user/login";
+    public static final String EMPLOYEE_LOGIN = BASE_URL+"employee/login";
+    public static final String ADMIN_LOGIN = BASE_URL+"admin/login";
+}
--- app/src/main/java/com/ryms/airlinemanagement/Login.java
+++ app/src/main/java/com/ryms/airlinemanagement/Login.java
@@ -2,13 +2,136 @@ package com.ryms.airlinemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Login extends AppCompatActivity {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    EditText email, password;
+    RadioButton user, employee, admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        user = (RadioButton) findViewById(R.id.user);
+        employee = (RadioButton) findViewById(R.id.employee);
+        admin = (RadioButton) findViewById(R.id.admin);
+    }
+
+    public void goToFlights() {
+        Intent i = new Intent(this, Flights.class);
+        startActivity(i);
+    }
+
+    public void loginPressed(View view) {
+        Log.d("LOGIN", ("Login Pressed"));
+        final String emailTxt = email.getText().toString();
+        final String passTxt = password.getText().toString();
+
+        if (emailTxt == null || passTxt == null || emailTxt == "" || passTxt == "") {
+            System.out.println("WRONG INPUTS");
+            return;
+        }
+
+        OkHttpClient client = new OkHttpClient();
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("email", email.getText().toString());
+            jsonBody.put("password", password.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(String.valueOf(jsonBody), JSON);
+        String requestUrl;
+        if(user.isChecked() == true){
+            requestUrl = Config.USER_LOGIN;
+        }
+        else if(employee.isChecked() == true){
+            requestUrl = Config.EMPLOYEE_LOGIN;
+        }
+        else {
+            requestUrl = Config.ADMIN_LOGIN;
+        }
+
+        Log.d("LOGIN", ("Creating req with url: " + Config.USER_LOGIN));
+        Request request = new Request.Builder()
+                .url(requestUrl)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                if (e instanceof UnknownHostException) {
+                    System.out.println("Please check your Internet connection.");
+                } else {
+                    System.out.println("Error executing login HTTP req.: ");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                JSONObject jsonObject = null;
+                if (!response.isSuccessful()) {
+                    Login.this.runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Incorrect username or password!", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    String res = response.body().string();
+                    Log.d("JSON RES", res);
+                    try {
+                        jsonObject = new JSONObject(res);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    JSONObject message;
+                    int result;
+                    try {
+                        message = jsonObject.getJSONObject("message");
+                        result = Integer.parseInt(message.getString("@result"));
+                        if(result==1){
+                            goToFlights();
+                        }
+                        else{
+                            Log.d("Chalja","kuch nahi hona");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
